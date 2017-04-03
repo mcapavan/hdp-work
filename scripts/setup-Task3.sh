3bd53d99f36f4623b80077aa46e96446d894a259
mcapavan <mcapavan@git.com>
2017-04-03 13:46:54 +0100

    remove hdp25

--- scripts/setup-Task3.sh
+++ scripts/setup-Task3.sh
@@ -9,7 +9,7 @@ echo "Download Hive-JSON-Serde from git"
 git clone https://github.com/rcongiu/Hive-JSON-Serde.git
 
 cd Hive-JSON-Serde
-mvn -Phdp25 clean package
+mvn clean package
 hdfs dfs -mkdir /lib
 hdfs dfs -put json-serde/target/json-serde-1.3.8-SNAPSHOT-jar-with-dependencies.jar /lib
 hdfs dfs -chmod -R 755 /lib
